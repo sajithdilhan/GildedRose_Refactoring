b6cf4e8d5422de6880b0dfd2046964511aca88d6
Sajith Dilhan <sajithdilhan@gmail.com>
2025-10-19 06:58:05 +0800

    Remove SellIn from Sulfuras

--- GildedRose/Sulfuras.cs
+++ GildedRose/Sulfuras.cs
@@ -2,8 +2,6 @@
 {
     public class Sulfuras : Item
     {
-        public int SellInFactor { get; set; } = 1;
-
         public Sulfuras(string Name, int SellIn, int Quality)
         {
             this.Name = Name;
